c55044ae37c441cfcc1e19c1d752faf6764b04d8
Asa White <whiteasa94@gmail.com>
2020-11-16 17:39:03 -0800

    added various fixes

--- scripts/main.js
+++ scripts/main.js
@@ -4,19 +4,30 @@ let backBtn = document.getElementById('backBtn');
 let pics = {
     puma:['pumaLogo.png','pumaShoe.jpg','pumaCloth.jpg'],
     nike:[],
-    champion:[],
+    champion:['champ1.jpg','champ2.jpg'],
     gymShark:[]
 }
 
 let current = 0;
 
 let next = ()=>{
-    current++;
-    if(current>2){
-        current = 0;
-        document.querySelector('.imageContainer').style.backgroundImage = `url(../images/${pics.puma[current]})`;
-    }else{
-        document.querySelector('.imageContainer').style.backgroundImage = `url(../images/${pics.puma[current]})`;
+    if(document.querySelector('.imageContainer').id==='puma'){
+        current++;
+        if(current>2){
+            current = 0;
+            document.querySelector('.imageContainer').style.backgroundImage = `url(../images/${pics.puma[current]})`;
+        }else{
+            document.querySelector('.imageContainer').style.backgroundImage = `url(../images/${pics.puma[current]})`;
+        }
+        }
+    if(document.querySelector('.imageContainer').id==='champion'){
+        current++;
+        if(current>1){
+            current = 0;
+            document.querySelector('.imageContainer').style.backgroundImage = `url(../images/${pics.champion[current]})`;
+        }else{
+            document.querySelector('.imageContainer').style.backgroundImage = `url(../images/${pics.champion[current]})`;
+        }
     }
 }
 
